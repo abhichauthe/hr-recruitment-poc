2d824409bb92553f7bef524d6d52f51c79d51f31
User <chauthabhishek@gmail.com>
2025-06-20 15:19:06 +0530

    Initial commit - Spring Boot project setup

---
+++ src/main/java/com/company/HrRecuritmentpoc/HrRecuritmentpocApplication.java
@@ -0,0 +1,13 @@
+package com.company.HrRecuritmentpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrRecuritmentpocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrRecuritmentpocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/HrRecuritmentpoc/HrRecuritmentpocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.HrRecuritmentpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrRecuritmentpocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
